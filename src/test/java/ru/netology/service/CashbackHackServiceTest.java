60f94d367a424cd2da1eac512aed8c8e536bf7db
Dyxxless <79612696784@yandex.ru>
2022-12-20 21:21:05 +0300

    Add tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,6 +1,8 @@
 package ru.netology.service;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
 
 class CashbackHackServiceTest {
@@ -8,20 +10,33 @@ class CashbackHackServiceTest {
     @Test
     void calculateBonus() {
         CashbackHackService service = new CashbackHackService();
-        int expected = 1000;
-        int amount = 2000;
-        int actual = service.remain(amount);
 
-        assertEquals(actual, expected);
+        int actual = service.remain(999);
+        int expected = 1;
+
+        Assert.assertEquals(actual, expected);
     }
-    @Test
-    void calculateBonus2() {
-        CashbackHackService service = new CashbackHackService();
-        int expected = 0;
-        int amount = 200;
-        int actual = service.remain(amount);
 
-        assertEquals(actual, expected);
+    @Test
+    public void shouldNotAsk() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturnRemainedInMidOfBoundary() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1500);
+        int expected = 500;
+
+        Assert.assertEquals(actual, expected);
     }
 
 }
