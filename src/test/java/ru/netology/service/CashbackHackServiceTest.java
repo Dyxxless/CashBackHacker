5c681f24ee35e0f65055a65eaa854aa462c89af3
Dyxxless <79612696784@yandex.ru>
2022-12-20 21:07:53 +0300

    Create JUnit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,27 +1,42 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
-import static org.testng.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 class CashbackHackServiceTest {
 
     @Test
-    void calculateBonus() {
-        CashbackHackService service = new CashbackHackService();
-        int expected = 1000;
-        int amount = 2000;
-        int actual = service.remain(amount);
+    public void shouldReturnRemained() {
 
-        assertEquals(actual, expected);
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(999);
+        int expected = 1;
+
+        Assert.assertEquals(actual, expected);
     }
-    @Test
-    void calculateBonus2() {
-        CashbackHackService service = new CashbackHackService();
-        int expected = 0;
-        int amount = 200;
-        int actual = service.remain(amount);
 
-        assertEquals(actual, expected);
+    // купил на 1000 ответ 0
+    @Test
+    public void shouldNotAskToIncrease() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturnRemainedInMidOfBoundary() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1500);
+        int expected = 500;
+
+        Assert.assertEquals(actual, expected);
     }
 
 }
