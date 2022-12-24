9dd3450a0a9707b406680295b573ce478e71ad4b
Dyxxless <79612696784@yandex.ru>
2022-12-24 21:34:54 +0300

    Edit test to crash

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -25,7 +25,7 @@ public class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
 
         int actual = service.remain(1000);
-        int expected = 1000;
+        int expected = 0;
 
         Assert.assertEquals(actual, expected);
     }
